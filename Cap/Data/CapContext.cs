9130e15c3082fa3dd7b68378d83ed493ddeb2651
Darrius <61365604+SixftEight@users.noreply.github.com>
2021-05-03 20:53:44 -0400

    update

---
+++ Cap/Data/CapContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cap.Models.Model;
+
+namespace Cap.Data
+{
+    public class CapContext : DbContext
+    {
+        public CapContext (DbContextOptions<CapContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cap.Models.Model.Task> Task { get; set; }
+    }
+}
---
+++ Cap/Models/Model/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cap.Models.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Date { get; set; }
+        
+    }
+}
